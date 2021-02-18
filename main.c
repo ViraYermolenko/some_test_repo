874bc0195bc4b5ec442d8582f5f23f9549093eff
v_yermolenko <yermolenko.vira@gmail.com>
2021-02-18 10:48:40 +0200

    My modified and debugged work

--- main.c
+++ main.c
@@ -4,12 +4,13 @@
 #include <string.h>
 
 typedef struct studentsMarks{
+    char group[35];
     char surname[35];
     char name[35];
     int mark1, mark2, mark3;
 }studMarks;
 
-struct studentsMarks data[20];
+//struct studentsMarks data[20];
 
 typedef struct listElement{
     studMarks data;
@@ -31,8 +32,12 @@ void deleteBySurname(struct listElement**, char []);
 //void deleteBySurname(struct listElement* , char);
 listElement *deleteElement(listElement**, listElement*);
 void freeElementMemory(listElement*);
+void writeListIntoFile(listElement* infData, const char *fileName);
+void writeListIntoFileOrderByGroupAndSurname(listElement*);
+void printListBadStudents(struct listElement*);
 
-//listElement *listBegin, *listEnd;
+void sortByGroupAndSurname(struct listElement*);
+
 
 int main() {
 
@@ -48,7 +53,7 @@ int main() {
     int menu = -1;
 
     while(menu != 0){
-        fflush(stdin);
+        fflush(stdin); //system("cls");
         printList(infData);
 
         printf("\nMenu:\n\n");
@@ -56,6 +61,9 @@ int main() {
         printf("2. Remove student\n");
         printf("3. Sort students by marks\n");
         printf("4. Remember list of marks in new file\n");
+        printf("5. Sort list order by group and surname\n");
+        printf("6. Write list into file ordered by group and surname\n");
+        printf("7. Print bad student\n");
         printf("0. Exit\n\n > ");
 
         scanf("%d",&menu);
@@ -64,11 +72,14 @@ int main() {
             studMarks marks;
             printf("\n\nAdding new student and marks:\n\n");
 
+            printf("Group > ");
+            scanf("%s",marks.group);
+
             printf("Surname > ");
-            scanf("%s",&marks.surname);
+            scanf("%s",marks.surname);
 
             printf("Name > ");
-            scanf("%s",&marks.name);
+            scanf("%s",marks.name);
 
             printf("mark 1 > ");
             scanf("%d",&marks.mark1);
@@ -89,13 +100,26 @@ int main() {
             printf("Print surname of a student:");
             scanf("%s", temp);
             deleteBySurname(&infData, temp);
-        }
-        else if(menu == 3)
-        {
+
+        } else if(menu == 3) {
             sortList(infData);
-        }
-        else if(menu == 0)
-        {
+
+        } else if(menu == 4) {
+            printf("File name > ");
+            scanf("%s", fileName);
+            writeListIntoFile(infData,fileName);
+
+        } else if(menu == 5) {
+            sortByGroupAndSurname(infData);
+
+        } else if(menu == 6) {
+            writeListIntoFileOrderByGroupAndSurname(infData);
+
+        } else if(menu == 7) {
+            printListBadStudents(infData);
+            system("pause");
+
+        } else if(menu == 0) {
             freeList(&infData);
         }
     }
@@ -106,19 +130,22 @@ int main() {
 
 void readListFromFile(listElement** infData, const char *fileName) {
 
-    char i = 0;
+    //char i = 0;
     FILE* file = fopen(fileName,"r");
     if(!file)
         return;
 
-    int numberX, numberY, numberZ;
+    ///int numberX, numberY, numberZ;
     while(!feof(file)) {
-        fscanf(file,"%s %s %d %d %d", &(data[i].surname), &(data[i].name), &(data[i].mark1), &(data[i].mark2), &(data[i].mark3));
+        struct studentsMarks studentsMark;
+
+        fscanf(file,"%s %s %s %d %d %d", (studentsMark.group), (studentsMark.surname),
+               (studentsMark.name), &(studentsMark.mark1), &(studentsMark.mark2), &(studentsMark.mark3));
+
         if(!feof(file))
-            addInTail(infData, data[i]);
+            addInTail(infData, studentsMark);
     }
     fclose(file);
-
 }
 
 void addInTail(listElement ** head, struct studentsMarks data) {
@@ -143,15 +170,20 @@ void addInTail(listElement ** head, struct studentsMarks data) {
 }
 
 void printList(struct listElement* head){
-    printf("Marks list:\n\nSurname       Name           Mark 1   Mark 2   Mark 3\n");
+    printf("Marks list:\n\n");
+    printf("X-------------------------------------------------------------------------X\n");
+    printf("| Group      | Surname       | Name         | Mark 1  | Mark 2  | Mark 3  |\n");
+    printf("X-------------------------------------------------------------------------X\n");
 
     int i = 0;
     for(listElement * current = head;current!=NULL;current = current->next){
-        printf("%-13s %-10s %8d %8d %8d \n",current->data.surname, current->data.name,
+        printf("| %-10s | %-14s| %-13s| %-8d| %-8d| %-8d|\n",current->data.group,current->data.surname, current->data.name,
                current->data.mark1, current->data.mark2, current->data.mark3);
+        printf(" -------------------------------------------------------------------------\n");
     }
     printf("\n");
 }
+
 
 
 int averageSum(studMarks marks){
@@ -159,6 +191,27 @@ int averageSum(studMarks marks){
      average= (marks.mark1 + marks.mark2 + marks.mark3)/3;
      return average;
 }
+
+void printListBadStudents(struct listElement* head)
+{
+    printf("Bad students list:\n\n");
+    printf("X-------------------------------------------------------------------------X\n");
+    printf("| Group      | Surname       | Name         | Mark 1  | Mark 2  | Mark 3  |\n");
+    printf("X-------------------------------------------------------------------------X\n");
+
+    int i = 0;
+    for(listElement * current = head;current!=NULL;current = current->next){
+        if(averageSum(current->data) < 60)
+        {
+            printf("| %-10s | %-14s| %-13s| %-8d| %-8d| %-8d|\n",current->data.group,current->data.surname, current->data.name,
+                   current->data.mark1, current->data.mark2, current->data.mark3);
+
+            printf(" -------------------------------------------------------------------------\n");
+        }
+    }
+    printf("\n");
+}
+
 
 void sortList(struct listElement* head){
     for(listElement * current = head; current!=NULL;current = current->next){
@@ -195,26 +248,22 @@ void freeList(listElement ** head){
 }
 
 void deleteBySurname(struct listElement** head, char temp[]) {
-    if(head != NULL && *head != NULL)
-    {
+    if(head != NULL && *head != NULL) {
+
         listElement* current = *head;
 
-        while(current != NULL && strcmp(current->data.surname,temp) != 0)
-        {
+        while(current != NULL && strcmp(current->data.surname,temp) != 0) {
             current = current->next;
         }
 
-        if(current != NULL)
-        {
-            if(current == *head)
-            {
+        if(current != NULL) {
+            if(current == *head) {
                 *head = (*head)->next;
                 (*head)->prev = NULL;
 
                 free(current);
-            }
-            else
-            {
+
+            } else {
                 current->prev->next = current->next;
                 if(current->next != NULL)
                     current->next->prev = current->prev;
@@ -225,10 +274,120 @@ void deleteBySurname(struct listElement** head, char temp[]) {
     }
 }
 
+listElement *deleteElement(listElement **head, listElement * pdel) {
+    if (head != NULL) {
+        if (*head != NULL) {
+            if (pdel == *head) {    //delete first element from list
+                *head = (*head)->next;
+                (*head)->prev = NULL;
+                freeElementMemory(pdel);
+                return head;
+                //return NULL;
+            }}}
+    listElement  *tail;
+    if (pdel == tail) {      //delete first element from list
+        tail = tail->prev;
+        tail->next = NULL;
+        freeElementMemory(pdel);
+        return NULL;
+    } else {                    //delete middle element from list
+        listElement *pnext;
+        pdel->next->prev = pdel->prev;
+        pnext = pdel->prev->next = pdel->next;
+        freeElementMemory(pdel);
+        return NULL;
+        //return pnext;
+    }
+}
+
+
+
+void writeListIntoFile(listElement* infData, const char *fileName) {
+
+    FILE* file = fopen(fileName,"w");
+
+    if(!file)
+        return;
+
+    listElement* current = infData;
+
+    while(current != NULL) {
+        fprintf(file,"%s %s %s %d %d %d\n",current->data.group,current->data.surname,current->data.name,
+                current->data.mark1,current->data.mark2,current->data.mark3);
+        current = current->next;
+    }
+    fclose(file);
+}
+
+void writeListIntoFileOrderByGroupAndSurname(listElement* infData) {
+    //copy list (if list non-sorted)
+    listElement* bufferList = NULL;
+    listElement* current = infData;
+
+    while(current != NULL) {
+        addInTail(&bufferList, current->data);
+        current = current->next;
+    }
+
+    //sorting order group
+    sortByGroupAndSurname(bufferList);
+
+    //writing in files
+    current = bufferList;
+    while(current != NULL) {
+        char fileName[255];
+        fileName[0] = '\0';
+
+        strcat(fileName, current->data.group);
+        strcat(fileName, ".txt");
+
+        FILE* file = fopen(fileName ,"w");
+
+        do {
+            fprintf(file,"%s %s %d %d %d\n",current->data.surname,current->data.name,
+                    current->data.mark1,current->data.mark2,current->data.mark3);
+
+            current = current->next;
+        } while(current != NULL && strcmp(current->data.group,current->prev->data.group) == 0);
+
+        fclose(file);
+    }
+
+    freeList(&bufferList);
+}
+
+void sortByGroupAndSurname(struct listElement* head) {
+
+    for(listElement * current = head; current!=NULL;current = current->next) {
+
+        for(listElement * scurrent = current->next;scurrent!=NULL;scurrent = scurrent->next) {
+
+            if(strcmp(current->data.group, scurrent->data.group) > 0 ||
+               strcmp(current->data.group, scurrent->data.group) == 0 &&
+               strcmp(current->data.surname, scurrent->data.surname) > 0) {
+
+                studMarks buffer = current->data;
+                current->data = scurrent->data;
+                scurrent->data = buffer;
+            }
+        }
+    }
+}
+
+
+
+
 void freeElementMemory(listElement * pel) {
     free(pel->data.surname);
     free(pel);
 }
+
+
+
+
+
+
+
 /*void deleteBySurname(struct listElement** head, char temp[]) {
     listElement * pel = head;
     printf("%s, %s\n", pel->data.surname, temp);
@@ -274,7 +433,7 @@ listElement *deleteElement(listElement **head, listElement * pdel) {
 
 
 // C:\Users\ThinkPad\CLionProjects\progDoubleList\file.txt
-//
+// C:\Users\ThinkPad\CLionProjects\progDoubleList\file2.txt
 //
 // /*listElement * temp = head;
 //    while ((temp != NULL) && (temp->data != surname[20]))
